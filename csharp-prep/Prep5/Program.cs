0e1b032ae4c77dd30a16d95a127703ad3cea0b0e
gloryakobo <gloryakobo279@gmail.com>
2024-09-30 17:03:02 +0100

    prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
-{
+{   
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine();
+        
+        DisplayWelcome();
+        Console.WriteLine();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        Console.WriteLine();
+        DisplayResult(userName, userNumber);
+
+        Console.WriteLine();
     }
+
+    // displays welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Ask the user for their name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    // Ask the user for their favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+
+        int userNumber = Convert.ToInt16(Console.ReadLine());
+        return userNumber * userNumber;
+    }
+
+    // Display both the name and square of their fav number.
+    static void DisplayResult(string userName, int userNumber)
+    {
+        Console.WriteLine($"Brother {userName}, the square of your number is {userNumber}");
+    }
+
+
 }
